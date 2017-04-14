38156f7d204056de3adb7051355b1ba4a35656cf
jmsmrgn <hello@jmsmrgn.com>
2017-04-14 14:41:17 -0700

    04 - Webpack, React, and Hot Module Replacement

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,5 +1,10 @@
 // @flow
 
-export const WEB_PORT = process.env.port || 8000
+export const WEB_PORT = process.env.port || 7777
 export const STATIC_PATH = '/static'
 export const APP_NAME = 'Hello App'
+
+export const WDS_PORT = 7666
+
+export const APP_CONTAINER_CLASS = 'js-app'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
